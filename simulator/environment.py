f95457ba8eaa53901d77653d5fdb7c04231fd1c3
jeevandas-jd <jeevandasms2004@gmail.com>
2026-01-16 18:30:58 +0530

    agregatorImplimentation

---
+++ simulator/environment.py
@@ -0,0 +1,46 @@
+
+class Room:
+
+    def __init__(self,room_id,room_name):
+
+        self.room_id=room_id
+        self.room_name=room_name
+
+        #environment variable setting
+
+        self.state={
+            "temprature":2000,
+            "humidity":2000,
+            "air_quality":2000,
+            "illuminance":50
+        }
+
+        self.devices={}
+
+class MatterDevices:
+
+    def __init__(self,device_id,device_type):
+        self.device_id=device_id
+        self.device_type=device_type
+
+        self.endpoints={}
+
+    def addEndPoint(self,endPoint_id):
+        self.endpoints[endPoint_id]=Endpoint(endPoint_id)
+
+
+class Endpoint:
+
+    def __init__(self,endpoint_id):
+        self.endpoint_id=endpoint_id
+        self.cluster={}
+
+
+class Cluster:
+
+    def __init__(self,name):
+        self.name=name
+
+        self.attributes={}
+        self.commanfs=[]
+
